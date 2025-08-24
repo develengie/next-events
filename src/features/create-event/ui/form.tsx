2d8e004c04d56b4d4db46869d5392e0dc0a2decc
Alexey Ushakov <develengie@gmail.com>
2025-08-24 16:12:47 +0300

    chore: app design adjusted

--- src/features/create-event/ui/form.tsx
+++ src/features/create-event/ui/form.tsx
@@ -26,9 +26,6 @@ export const CreateEventForm = ({ onSubmit }: CreateEventFormProps) => {
                     <h2 className="text-base font-semibold leading-7 text-gray-900">
                         Событие
                     </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-600">
-                        Заполните форму для создания события
-                    </p>
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div className="sm:col-span-4">
